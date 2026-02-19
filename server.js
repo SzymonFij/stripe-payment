fedf960395e288b50ea25ee078a85c515b63300b
SzymonFij <sfijalkowski03@gmail.com>
2026-02-19 21:24:01 +0100

    server cors added

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const Stripe = require('stripe');
 const app = express();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTENT_URL
+}));
 app.use(express.json());
 
 app.post('/create-payment-intent', async (req, res) => {
