2a93bb4242ea8b7b5c6482719e49b6679bbb2405
SzymonFij <sfijalkowski03@gmail.com>
2026-02-27 20:49:14 +0100

    payment status with additional information sent via api

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -23,11 +23,25 @@ router.get(
             );
 
             if (result.rows.length === 0) {
-                return res.status(404).json({ error: "User not found" });
+                try {
+                    const linkRes = await pool.query(
+                        `SELECT * FROM payment_links WHERE email=$1`,
+                        [email]
+                    );
+                    if (linkRes.rows.length === 0) {
+                        return res.status(404).json({ error: "No payment link was found"});
+                    }
+                    if (linkRes.rows[0].used) {
+                        res.json({ status: "Payment link used, but payment not found. Generate new link."});
+                    }
+                    res.json({ status: "Payment link created, waiting for payment."});
+                } catch {
+                    return res.status(404).json({ error: "User not found" });
+                }
             }
 
             console.log("Check if only one payment was created", result.rows);
-            res.json({ status: result.rows[0].payment_status });
+            res.json({ status: result.rows[0].status });
         } catch (error) {
             res.status(500).json({ error: "Database error"});
         }
